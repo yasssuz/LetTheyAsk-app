d9170201a7fc0c0447d878c215eec64ef2d512a4
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-17 21:36:08 -0300

    working on

---
+++ src/components/home/FilterSquare.js
@@ -0,0 +1,11 @@
+import { Box, Heading, List } from "@chakra-ui/layout";
+
+export default function FilterSquare() {
+  return (
+    <Box as='section'>
+      <List>
+        <Heading>still developing</Heading>
+      </List>
+    </Box>
+  );
+}

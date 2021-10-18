1d12b9ed2d49330b7150a64293ac265b2fd61695
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-17 21:27:36 -0300

    added theming

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,32 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "../globals.css";
+
+const theme = extendTheme({
+  textStyles: {
+    body: {
+      fontFamily: "serif",
+    },
+  },
+  colors: {
+    darkGray: "#373F68",
+    darkBlue: "#3A4374",
+    blue: "#4661E6",
+    cyan: "#62BCFA",
+    purple: "#AD1FEA",
+    orange: "#F49F85",
+    darkGray: "#647196",
+    gray: "#F2F4FF",
+    lightGray: "#F7F8FD",
+    white: "#FFFFFF",
+  },
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;

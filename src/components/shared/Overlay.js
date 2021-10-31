c61eb969822fc9ca3ba6af2544ead6a4cb3920cd
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-30 22:35:02 -0300

    overlay created

---
+++ src/components/shared/Overlay.js
@@ -0,0 +1,19 @@
+import { Box } from "@chakra-ui/layout";
+
+export default function Overlay({ show }) {
+  console.log(show);
+  return (
+    <Box
+      position='fixed'
+      top='0'
+      bottom='0'
+      right='0'
+      left='0'
+      bg='rgba(0, 0, 0, 0.5)'
+      zIndex='1000'
+      visibility={show ? "visible" : "hidden"}
+      opacity={show ? "1" : "0"}
+      transition='opacity 0.45s ease, visibility 0.45s ease'
+    />
+  );
+}

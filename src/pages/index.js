263eca09243c279f9712d59fd3c81ecf749203c6
Karim Yassine <karimwebdevelopment@gmail.com>
2021-10-31 21:42:01 -0300

    added responsivness

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
 import SideArea from "../components/home/SideArea";
 
 export default function Home() {
   return (
-    <Box>
+    <Grid
+      as='main'
+      templateColumns={[null, null, "25.5rem 100%"]}
+      w='100%'
+      minH='100vh'
+      p={[null, null, "9.4rem 4rem"]}
+    >
       <SideArea />
-    </Box>
+    </Grid>
   );
 }

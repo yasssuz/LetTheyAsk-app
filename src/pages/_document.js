089888d01900687189faefdbefbc788b0f9d1819
Karim Yassine <karimwebdevelopment@gmail.com>
2021-11-01 22:18:11 -0300

    change to functional component

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,27 +1,24 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Jost:wght@400;600;700&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Jost:wght@400;600;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <body style={{ background: "#F7F8FD" }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }

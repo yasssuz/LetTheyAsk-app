27b15631e084358bdbf5bc73d3f8a2218a914efd
Karim Yassine <karimwebdevelopment@gmail.com>
2021-11-03 09:47:59 -0300

    added GoBack and Card component

---
+++ src/pages/create-feedback.js
@@ -0,0 +1,22 @@
+import { Box, Flex } from "@chakra-ui/layout";
+
+import Card from "../components/createFeedback/Card";
+import GoBack from "../components/shared/GoBack";
+
+export default function createFeedback() {
+  return (
+    <Flex
+      as='main'
+      minH='100vh'
+      flexDirection={[null, "column"]}
+      alignItems={[null, "center"]}
+      justifyContent={[null, "center"]}
+      p='3.4rem 2.4rem 4rem'
+    >
+      <Box w='100%' maxW='72rem'>
+        <GoBack />
+        <Card />
+      </Box>
+    </Flex>
+  );
+}

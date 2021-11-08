f2bb64868291e6265834609942b6da8fff8cda33
Karim Yassine <karimwebdevelopment@gmail.com>
2021-11-07 21:46:27 -0300

    add signIn and authStateChanged functions

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,60 @@
+import { createContext, useEffect, useState } from "react";
+
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "@firebase/auth";
+import { auth } from "../services/firebase.config";
+
+export const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  async function handleSignIn() {
+    const provider = new GoogleAuthProvider();
+    const res = await signInWithPopup(auth, provider);
+
+    if (!res.user.displayName || !res.user.photoURL)
+      throw new Error("missing google information");
+
+    const formattedUser = {
+      name: res.user.displayName,
+      avatar: res.user.photoURL,
+      id: res.user.uid,
+    };
+
+    setUser(formattedUser);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      if (!user.displayName || !user.photoURL)
+        throw new Error("missing google information");
+
+      const formattedUser = {
+        name: user.name,
+        avatar: user.avatar,
+        id: user.id,
+      };
+
+      setUser(formattedUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        handleSignIn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}

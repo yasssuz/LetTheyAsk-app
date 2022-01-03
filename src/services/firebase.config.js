aeaaec2514c98030201d7d117ffbf244fb449a3b
Karim Yassine <karimwebdevelopment@gmail.com>
2022-01-02 21:38:09 -0300

    export app initialization

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -12,6 +12,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_API_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
